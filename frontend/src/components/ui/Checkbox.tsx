7f2fb2956c873bb427478c8709bd24061e80aeda
Asogwa Chukwuemeka <101509899+Mekazstan@users.noreply.github.com>
2025-04-21 21:43:53 +0100

    Update Checkbox.tsx

--- frontend/src/components/ui/Checkbox.tsx
+++ frontend/src/components/ui/Checkbox.tsx
@@ -1,25 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, InputHTMLAttributes } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
 
-interface CheckboxProps {
-    children?: React.ReactNode;
-    className?: string;
+interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+  children?: React.ReactNode;
+  className?: string;
+  register?: UseFormRegisterReturn;
 }
 
-export const Checkbox: FC<CheckboxProps> = ({ children, className }) => {
-    return (
-        <div
-            className={`${
-                className && className
-            } flex flex-row justify-start gap-2 w-full items-center mb-2`}
-        >
-            <input
-                type="checkbox"
-                name=""
-                id=""
-                className="accent-[#5c5ce9] effect"
-                required
-            />
-            <p>{children}</p>
-        </div>
-    );
+export const Checkbox: FC<CheckboxProps> = ({ 
+  children, 
+  className, 
+  register,
+  ...props 
+}) => {
+  return (
+    <div
+      className={`${
+        className ? className : ""
+      } flex flex-row justify-start gap-2 w-full items-center mb-2`}
+    >
+      <input
+        type="checkbox"
+        className="accent-[#5c5ce9] effect"
+        {...register}
+        {...props}
+      />
+      <p>{children}</p>
+    </div>
+  );
 };
